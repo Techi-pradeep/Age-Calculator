d601b4a24ad230afe28a4b40c91dd69aa22efafd
Techi-pradeep <pradeepk259909@gmail.com>
2023-01-23 17:17:33 +0530

    first

---
+++ Blood Donation Form.html/blood.js
@@ -0,0 +1,27 @@
+//code for regular expression for user name ?
+
+
+
+var usernameRegex = /^[a-zA-Z0-9]+$/;
+
+
+
+
+
+//code for regular expression for user phone number ?
+^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
+
+
+123-456-7890
+(123) 456-7890
+123 456 7890
+123.456.7890
++91 (123) 456-7890
+
+
+^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
+
+
+^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$
+
+
